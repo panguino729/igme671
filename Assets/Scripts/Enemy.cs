67e9ec63373fc244ed585456a8049b432a788c14
Van Edelman <vanedelman@gmail.com>
2020-11-08 19:44:24 -0500

    added some basic scripts

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Player : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
