564e2db7a5895fad9e5b44aa45d9ddd1761e8cf6
al8249 <al8249@rit.edu>
2020-12-06 18:27:11 -0500

    Finished lvl 4

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,6 +15,6 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -15);
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -25);
     }
 }
