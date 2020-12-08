8f29fb8a68c09dc73fe4138d0675bc43e8188f99
lcr3602 <lcr3602@g.rit.edu>
2020-12-08 17:37:22 -0500

    Removed custom editor code for MenuButton

--- Assets/Scripts/Menu/MenuButton.cs
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -32,38 +31,5 @@ public class MenuButton : MonoBehaviour
             default:
                 break;
         }
-    }
-}
-
-[CustomEditor(typeof(MenuButton))]
-public class MenuButtonEditor : Editor
-{
-    SerializedProperty buttonType;
-    SerializedProperty sceneToLoad;
-    SerializedProperty nextMenuPos;
-
-    void OnEnable()
-    {
-        buttonType = serializedObject.FindProperty("buttonType");
-        sceneToLoad = serializedObject.FindProperty("sceneToLoad");
-        nextMenuPos = serializedObject.FindProperty("nextMenuPos");
-    }
-
-    public override void OnInspectorGUI()
-    {
-        serializedObject.Update();
-        EditorGUILayout.PropertyField(buttonType);
-        switch(buttonType.intValue)
-        {
-            case 1:
-                EditorGUILayout.PropertyField(sceneToLoad);
-                break;
-            case 2:
-                EditorGUILayout.PropertyField(nextMenuPos);
-                break;
-            default:
-                break;
-        }
-        serializedObject.ApplyModifiedProperties();
     }
 }
